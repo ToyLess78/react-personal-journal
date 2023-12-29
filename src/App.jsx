c01e03de518cc6c597bf19de6c909f1908046175
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2023-12-29 21:53:10 +0200

    Part 5: Displaying Data > Display by condition - second way

--- src/App.jsx
+++ src/App.jsx
@@ -27,23 +27,25 @@ function App() {
 		}
 		return -1;
 	};
-
+	let list = <p>There are no posts yet, add the first one.</p>;
+	if (items.length > 0) {
+		list = items.sort(sortItems).map(el => (
+			<CardButton key={el.id}>
+				<JournalItem
+					title={el.title}
+					text={el.text}
+					date={el.date}
+				/>
+			</CardButton>
+		));
+	}
 	return (
 		<div className='app'>
 			<LeftPanel>
 				<Header />
 				<JournalAddButton />
 				<JournalList>
-					{items.length === 0 && <p>There are no posts yet, add the first one.</p> }
-					{items.length > 0 && items.sort(sortItems).map(el => (
-						<CardButton key={el.id}>
-							<JournalItem
-								title={el.title}
-								text={el.text}
-								date={el.date}
-							/>
-						</CardButton>
-					))}
+					{list}
 				</JournalList>
 
 			</LeftPanel>
