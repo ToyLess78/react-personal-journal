f7f53743780823b91e1beffdf528a004cd292936
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-01-01 19:51:41 +0200

    Part 7: Other hooks > useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,30 @@
 import './App.css';
-import {INITIAL_DATA} from './INITIAL_DATA.js';
+// import {INITIAL_DATA} from './INITIAL_DATA.js';
 import {LeftPanel} from './layouts/LeftPanel/LeftPanel.jsx';
 import {Body} from './layouts/Body/Body.jsx';
 import Header from './components/Header/Header.jsx';
 import {JournalList} from './components/JournalList/JournalList.jsx';
 import {JournalAddButton} from './components/JournalAddButton/JournalAddButton.jsx';
 import {JournalForm} from './components/JournalForm/JournalForm.jsx';
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
+
 function App() {
 
-	const [items, setItems] = useState(INITIAL_DATA);
+	const [items, setItems] = useState([]);
+
+	useEffect(() => {
+		const  data = JSON.parse(localStorage.getItem('data'));
+		if (data) {
+			setItems(data.map(item => ({
+				...item,
+				date: new Date(item.date)
+			})));
+		}
+	}, []);
 
 	const addItem = item => {
 		setItems(oldItems => [...oldItems, {
-			text: item.text,
+			post: item.post,
 			title: item.title,
 			date: new Date(item.date),
 			id: oldItems.length > 0 ? Math.max(...oldItems.map(i => i.id)) + 1 : 1
