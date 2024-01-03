64ef85e122d4abe85dbc6a1c9bbe9a2273304317
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-01-03 20:06:50 +0200

    Part 9: React work > useCallback

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,14 @@
 import SelectUser from '../SelectUser/SelectUser.jsx';
 import Button from '../Button/Button.jsx';
-import {useState} from 'react';
+import {useCallback, useState} from 'react';
 import Logo from '../Logo/Logo.jsx';
 
 const logos = ['logo.svg', 'vite.svg'];
 function Header() {
 	const [logoIndex, setLogoIndex] = useState(0);
-	const toggleLogo = () => {
+	const toggleLogo = useCallback(() => {
 		setLogoIndex(state => Number(!state));
-	};
+	}, []);
 
 	return (
 		<>
