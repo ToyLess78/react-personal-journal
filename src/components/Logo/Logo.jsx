2807347aede03248f22b4cafb2f0288626126f4c
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-01-03 19:57:02 +0200

    Part 9: React work > Memo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,10 +1,9 @@
 import styles from './Logo.module.css';
-// import { memo } from 'react';
+import { memo } from 'react';
 
-export default function Logo({ image }) {
-	console.log('Logo');
+function Logo({ image }) {
 
 	return <img className={styles.logo} src={image} alt='Logo' />;
 }
 
-// export default memo(Logo);
+export default memo(Logo);
